be9298b239dd30210a4a1f3f5b39398d10106bbb
HemantSakhare <sakhare.hemant@gmail.com>
2019-05-12 07:32:04 +0530

    First Change

---
+++ jenkins_declarative_pipeline/src/main/java/com/example/jenkins_declarative_pipeline/JenkinsDeclarativePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_declarative_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeclarativePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDeclarativePipelineApplication.class, args);
+	}
+
+}
---
+++ jenkins_declarative_pipeline/src/test/java/com/example/jenkins_declarative_pipeline/JenkinsDeclarativePipelineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jenkins_declarative_pipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsDeclarativePipelineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
